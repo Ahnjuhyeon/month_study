3a303d126925e07c3e9368cf51f2318d45195a5c
sgm4247 <sgm4247@naver.com>
2024-02-02 22:32:37 +0900

    feat : 다시 data 불러오기 중

---
+++ issues/src/apis/core.js
@@ -0,0 +1,53 @@
+import { Octokit } from "octokit";
+import { useEffect } from "react";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const getRepoIssues = async () => {
+  const owner = "angular";
+  const repo = "angular-cli";
+  const perPage = 50;
+
+  try {
+    const response = await octokit.request(
+      `GET /repos/${owner}/${repo}/issues`,
+      {
+        owner: owner,
+        repo: repo,
+        per_page: perPage,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    const data = response.data;
+    console.log(data); // 데이터 확인을 위한
+    return data;
+  } catch (error) {
+    console.error("Error fetching repo issues:", error.message);
+    throw error;
+  }
+};
+
+const Page = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getRepoIssues();
+        // 데이터를 사용하는 작업 추가
+        console.log("Data received:", data);
+      } catch (error) {
+        // 오류 처리
+        console.error("Error in fetchData:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []); // 변경: 불필요한 함수 래핑 제거
+
+  return <>dd</>;
+};
+
+export default Page;
