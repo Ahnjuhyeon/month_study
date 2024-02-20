ccc23925265d87e871ecd4a29351464b40e9a9ed
sgm4247 <sgm4247@naver.com>
2024-02-20 23:01:25 +0900

    feat : issues / loginPage 추가

---
+++ issues/src/pages/login/login.js
@@ -0,0 +1,4 @@
+const LoginPage = () => {
+  return <>slsisksy</>;
+};
+export default LoginPage;
--- issues/src/route/route.js
+++ issues/src/route/route.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/main/main";
 import DetailPage from "../pages/detail/detail";
+import LoginPage from "../pages/login/login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
+  },
+  {
+    path: "/loginPage",
+    element: <LoginPage />,
   },
   {
     path: "/detailPage",
