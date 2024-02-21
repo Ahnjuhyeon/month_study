0cd9ce24ba15aecb36bbb95fe046f71b65c6f8f0
sgm4247 <sgm4247@naver.com>
2024-02-21 23:58:14 +0900

    feat : apis/issues login 추가

--- issues/src/App.js
+++ issues/src/App.js
@@ -1,19 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import MainPage from "./pages/main/main";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./route/route";
-import AuthProvider from "./provider/auth-provider";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router}>
-          <MainPage />
-        </RouterProvider>
-      </AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
     </QueryClientProvider>
   );
 }
--- issues/src/apis/core.js
+++ issues/src/apis/core.js
@@ -1,33 +1,8 @@
 import axios from "axios";
-import TokenRepository from "../repository/token-repository";
 
 export const axiosInstance = axios.create({
   baseURL: "https://api.github.com/repos/angular/angular-cli/issues",
   // headers: {
   //   Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
   // },
-});
-
-// export const axiosLoginInstance = axios.create({
-//   baseURL: "https://topdragon.co.kr",
-//   headers: {
-//     Authorization: `Bearer ${TokenRepository.getToken()}`,
-//   },
-//   withCredentials: true,
-// });
-
-export const axiosLoginInstance = () => {
-  return axios.create({
-    baseURL: "https://topdragon.co.kr",
-    headers: {
-      Authorization: `Bearer ${TokenRepository.getToken()}`,
-    },
-    withCredentials: true,
-  });
-};
-
-axiosLoginInstance.interceptors.request.use(function (config) {
-  const token = TokenRepository.getToken();
-  config.headers.Authorization = token ? `Bearer ${token}` : "";
-  return config;
 });
